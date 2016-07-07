e473056bd4830c229c255dbd301237e684e6443e
ivoiv8 <ivoiv8@gmail.com>
2016-07-08 02:20:28 +0300

    Initial Commit

---
+++ FlickrDemo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FlickrDemo
+//
+//  Created by Ivaylo Ivanov on 8.07.16 г..
+//  Copyright © 2016 г. Ivaylo Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ FlickrDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlickrDemo
+//
+//  Created by Ivaylo Ivanov on 8.07.16 г..
+//  Copyright © 2016 г. Ivaylo Ivanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
